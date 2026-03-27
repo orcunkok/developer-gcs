81b1b65b5b38641769f5a3c5d894868d9c713921
orcun.kok <orcunkok333@gmail.com>
2026-03-26 23:30:20 -0700

    collapsible bottom and right pane

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <script setup>
+import { ref, onMounted, onUnmounted } from 'vue'
 import StatusBar from './components/StatusBar.vue'
 import LeftPane from './components/LeftPane.vue'
 import PrimaryDisplay from './components/PrimaryDisplay.vue'
 import RightPane from './components/RightPane.vue'
 import BottomStrip from './components/BottomStrip.vue'
+
+const rightPaneOpen = ref(true)
+
+function handleKeydown(e) {
+  if (e.ctrlKey && e.key === '.') {
+    e.preventDefault()
+    rightPaneOpen.value = !rightPaneOpen.value
+  }
+}
+
+onMounted(() => window.addEventListener('keydown', handleKeydown))
+onUnmounted(() => window.removeEventListener('keydown', handleKeydown))
 </script>
 
 <template>
-  <div class="app-shell">
+  <div class="app-shell" :class="{ 'right-collapsed': !rightPaneOpen }">
     <StatusBar />
     <LeftPane />
     <PrimaryDisplay />
-    <RightPane />
+    <RightPane v-if="rightPaneOpen" />
     <BottomStrip />
   </div>
 </template>
@@ -28,5 +41,13 @@ import BottomStrip from './components/BottomStrip.vue'
   grid-template-columns: var(--left-pane-width) 1fr var(--right-pane-width);
   grid-template-rows: var(--topbar-height) 1fr var(--bottom-strip-height);
   overflow: hidden;
+}
+
+.app-shell.right-collapsed {
+  grid-template-areas:
+    "topbar  topbar"
+    "left    primary"
+    "bottom  bottom";
+  grid-template-columns: var(--left-pane-width) 1fr;
 }
 </style>
